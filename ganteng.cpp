f927278b0d47e522bde142b1805c1e3a41621810
shahidulfarid <118264004+shahidulfarid@users.noreply.github.com>
2022-11-15 17:17:19 +0700

    Add files via upload
    1 dimensi adalah program menginput nilai dan 2  dimensi adalah membentuk matriks

---
+++ ganteng.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	
+	int A[3][4]={{3,4,8,0},{3,9,2,1},{6,3,0,2}};
+	
+cout << "program array 2 dimensi \n Shahidul Farid \n 22343072 \n" << endl;
+cout << "pembuatan matriks" << endl;
+	for (int b=0;b<3;b++){
+		for (int k=0;k<4;k++){
+			cout<<A[b][k]<<" ";
+		}
+		cout << endl;
+	}
+getch();
+
+}
